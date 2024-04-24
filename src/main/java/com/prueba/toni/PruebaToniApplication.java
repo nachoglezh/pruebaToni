2fba451055deaeddfcda7703463de3a0c1cdfc6a
Nacho González <nachohgon99@gmail.com>
2024-04-24 18:59:50 +0200

    estructura inicial

---
+++ src/main/java/com/prueba/toni/PruebaToniApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.toni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaToniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaToniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prueba/toni/PruebaToniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.toni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaToniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
